c5c76b52401321d5f98d63eeaa551b6afad29ece
vingt <vingt@phenomenex.com>
2015-01-07 12:49:48 -0800

    -initial commit

---
+++ GitTestProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GitTestProject.MvcApplication" Language="C#" %>
---
+++ GitTestProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitTestProject.Startup))]
+namespace GitTestProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
